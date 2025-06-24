b8fd633e2ddecddb604f49dd3b98468d887446b5
Glen Kelley <gisaack@byui.edu>
2025-06-24 13:53:46 -0600

    testing js

--- src/js/main.js
+++ src/js/main.js
@@ -10,28 +10,28 @@ async function homebooks() {
         const randomBooks = getRandomBooks(booksInGenre, 3);
         displayBooks(randomBooks, genre);
     });
-    function getRandomBooks(arr, num) {
-        const shuffled = [...arr].sort(() => 0.5 - Math.random());
-        return shuffled.slice(0, num);
+}
+function getRandomBooks(arr, num) {
+    const shuffled = [...arr].sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, num);
+}
+function displayBooks(books, genre) {
+    const containerId = `${genre.toLowerCase().replace(/\s/g, '-')}-books`;
+    const container = document.querySelector(`#${containerId} .book-covers-container`);
+    if (!container) {
+        console.error(`Container not found for genre: ${genre}`);
+        return;
     }
-    function displayBooks(books, genre) {
-        const containerId = `${genre.toLowerCase().replace(/\s/g, '-')}-books`;
-        const container = document.querySelector(`#${containerId} .book-covers-container`);
-        if (!container) {
-            console.error(`Container not found for genre: ${genre}`);
-            return;
-        }
-        books.forEach(book => {
-            const bookItem = document.createElement('div');
-            bookItem.classList.add('book-cover-item');
-            const bookImage = document.createElement('img');
-            bookImage.src = book.cover;
-            bookImage.alt = book.title;
-            const bookTitle = document.createElement('p');
-            bookTitle.textContent = book.title;
-            bookItem.appendChild(bookImage);
-            bookItem.appendChild(bookTitle);
-            container.appendChild(bookItem);
-        });
-    }
-};
+    books.forEach(book => {
+        const bookItem = document.createElement('div');
+        bookItem.classList.add('book-cover-item');
+        const bookImage = document.createElement('img');
+        bookImage.src = book.cover;
+        bookImage.alt = book.title;
+        const bookTitle = document.createElement('p');
+        bookTitle.textContent = book.title;
+        bookItem.appendChild(bookImage);
+        bookItem.appendChild(bookTitle);
+        container.appendChild(bookItem);
+    });
+}
